025074bec745171d67e22fdc31edfbdfbd9acbe7
divyanshukumar10 <divyanshu.kr10@gmail.com>
2023-01-10 23:35:38 +0530

    helloworld

---
+++ Practice/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+class divyanshu
+{
+    static void main2()
+    {
+        Console.WriteLine("Hello main2");
+    }
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        main2();
+    }
+}
