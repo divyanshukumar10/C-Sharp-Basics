deaaf32537f57fd5a59c9996bb37c0e626e346cf
divyanshukumar10 <divyanshu.kr10@gmail.com>
2023-01-19 16:37:10 +0530

    code added

--- Practice/stringtype4/Program.cs
+++ Practice/stringtype4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name1 = "\"Divyanshu\"";
+string name2 = "Kumar";
+
+Console.WriteLine("{0} {1}",name1,name2);
+Console.WriteLine(name2);
+
+//Escape Sequence : \
+
+Console.WriteLine(@"!@!$!%!");
+
+// add @ in the starting of the string to print anything
