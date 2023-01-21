3667e35c67554fef8e164025aad63d5b8b0cea31
divyanshukumar10 <divyanshu.kr10@gmail.com>
2023-01-21 20:31:09 +0530

    code added

--- Practice/commonoperators5/Program.cs
+++ Practice/commonoperators5/Program.cs
@@ -1,4 +1,4 @@
-﻿// common operators like +,-,/,*
+﻿// arithmetic operators like +,-,/,*
 
 int a = 4;
 int b = 5;
@@ -7,4 +7,9 @@ Console.WriteLine(a + b);
 Console.WriteLine(a - b);
 Console.WriteLine(a / b);
 Console.WriteLine(a * b);
+
+if(a == 4)
+{
+    Console.WriteLine("The value of a is 4");
+}
 
