d6eb0ba20001c208d6b656741b4961caf1759ea0
divyanshukumar10 <divyanshu.kr10@gmail.com>
2023-01-21 20:09:58 +0530

    arrays

--- Practice/commonoperators5/Program.cs
+++ Practice/commonoperators5/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// common operators like +,-,/,*
+
+int a = 4;
+int b = 5;
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a / b);
+Console.WriteLine(a * b);
+
